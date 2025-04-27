40bf90744c029b3ee0ecc6e2d281115e68e1c4eb
Thanh Binh <52200104@student.tdtu.edu.vn>
2025-04-27 17:47:57 +0700

    web deploy

--- midterm/lib/main.dart
+++ midterm/lib/main.dart
@@ -2,13 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:midterm/views/AddToDoPage.dart';
 import 'package:midterm/views/Login.dart';
 import 'package:midterm/views/Register.dart';
+import 'package:midterm/views/VerificationPage.dart';
 
 import 'views/HomePage.dart';
 
 void main() {
   runApp(MaterialApp(
+    routes: {
+      '/': (context) => const HomePage(),
+      '/verify': (context) => const VerifyPage(),
+    },
     debugShowCheckedModeBanner: false,
     theme: ThemeData(),
-    home: HomePage(),
+    home: LoginPage(),
   ));
 }
